fa525966e87e76974400195eee3d3a714c069279
heerucan <iwpow0307@gmail.com>
2021-11-14 17:15:13 +0900

    [ADD] MainTabbar 설정 및 스보 분리 세팅(#5)

--- Tabling-iOS/Global/Constant/Constant.swift
+++ Tabling-iOS/Global/Constant/Constant.swift
@@ -5,4 +5,23 @@
 //  Created by Thisisme Hi on 2021/11/14.
 //
 
-import Foundation
+import UIKit
+
+struct Const {
+    
+    // MARK: - Icon
+    
+    public enum Icon {
+        public static let home = UIImage(named: "homeSelected")
+        public static let homeUnselected = UIImage(named: "homeUnselected")
+        
+        public static let wish = UIImage(named: "wishSelected")
+        public static let wishUnselected = UIImage(named: "wishUnselected")
+        
+        public static let calendar = UIImage(named: "calendarSelected")
+        public static let calendarUnselected = UIImage(named: "calendarUnselected")
+        
+        public static let mypage = UIImage(named: "mypageSelected")
+        public static let mypageUnselected = UIImage(named: "mypageUnselected")
+    }
+}
--- Tabling-iOS/Source/Screen/MyPage/MyPageVC.swift
+++ Tabling-iOS/Source/Screen/MyPage/MyPageVC.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-class MyPageVC: UIViewController {
-
+class MyPageVC: BaseViewController {
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Override Method UI + Layout
+    
+    override func configUI() {
+        super.configUI()
     }
-    */
-
+    
+    override func setupAutoLayout() {
+        super.setupAutoLayout()
+    }
+    
+    // MARK: - Custom Method
+    
 }
