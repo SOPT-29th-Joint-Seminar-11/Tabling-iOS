d1993a6c69e8a9b1a7f7b85415b5d3ef79f0aec4
ningpop <craft1933@naver.com>
2021-11-19 05:35:00 +0900

    [ADD] TableViewCell (Address) register (#11)

--- Tabling-iOS/Source/Screen/Namjoon/VC/NamjoonMainVC.swift
+++ Tabling-iOS/Source/Screen/Namjoon/VC/NamjoonMainVC.swift
@@ -10,25 +10,65 @@ import UIKit
 class NamjoonMainVC: UIViewController {
     
     // MARK: - Properties
-    let tableView = UITableView()
+    
+    let mainTV = UITableView()
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubviews([tableView])
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        
-        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
-        tableView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        tableView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
-        
-        tableView.register(NamjoonTVC.self, forCellReuseIdentifier: "NamjoonTVC")
+        configureUI()
+        setUpAutoLayout()
+        setUpTableView()
     }
     
     // MARK: - Custom Method
+    
+    func configureUI() {
+        // 모서리 둥글기나 그밖의 찐 설정...
+    }
+    
+    func setUpAutoLayout() {
+        view.addSubviews([mainTV])
+        
+        mainTV.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainTV.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        mainTV.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        mainTV.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        mainTV.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+    }
+    
+    func setUpTableView() {
+        mainTV.delegate = self
+        mainTV.dataSource = self
+        //tableView.register(NamjoonAddressTVC.self, forCellReuseIdentifier: NamjoonAddressTVC.className)
+        NamjoonAddressTVC.register(target: mainTV) // UITableViewRegisterable Extension을 활용해서 짧게!
+    }
+}
 
+// MARK: - UITableViewDelegate
+
+extension NamjoonMainVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 106
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension NamjoonMainVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NamjoonAddressTVC.className, for: indexPath) as? NamjoonAddressTVC
+        else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    
 }
