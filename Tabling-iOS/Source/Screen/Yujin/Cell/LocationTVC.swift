a0a546e9fbe0dab072497d3835f06a009791a701
heerucan <iwpow0307@gmail.com>
2021-11-20 03:31:42 +0900

    [ADD] LocationTVC 추가

---
+++ Tabling-iOS/Source/Screen/Yujin/Cell/LocationTVC.swift
@@ -0,0 +1,23 @@
+//
+//  LocationTVC.swift
+//  Tabling-iOS
+//
+//  Created by Thisisme Hi on 2021/11/20.
+//
+
+import UIKit
+
+class LocationTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
