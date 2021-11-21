3f2f0b266c2c74ddf015847c402b29fd13bd45c7
ningpop <craft1933@naver.com>
2021-11-21 22:33:18 +0900

    [ADD] TVC 파일 추가

---
+++ Tabling-iOS/Source/Screen/Namjoon/Cell/NJBannerTVC.swift
@@ -0,0 +1,23 @@
+//
+//  NJBannerTVC.swift
+//  Tabling-iOS
+//
+//  Created by 이남준 on 2021/11/21.
+//
+
+import UIKit
+
+class NJBannerTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
