a3ebe9d5f779e161b142cd9226fa475877a49cb8
Yujin Lee <yj587878@gmail.com>
2021-11-26 03:47:57 +0900

    [FEAT] 검색창 placeholder 및 돋보기 아이콘 추가(#10)

--- Tabling-iOS/Source/Screen/Yujin/Cell/SearchHeader.swift
+++ Tabling-iOS/Source/Screen/Yujin/Cell/SearchHeader.swift
@@ -17,7 +17,16 @@ class SearchHeader: UITableViewHeaderFooterView {
     
     let searchTextField: UITextField = {
         let textfield = UITextField()
+        textfield.addLeftPadding(width: 20)
+        textfield.addRightPadding(width: 20)
+        textfield.placeholder = "지역/음식종류/레스토랑 검색"
         return textfield
+    }()
+    
+    let searchIconImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = Const.Icon.search
+        return imageView
     }()
     
     override init(reuseIdentifier: String?){
@@ -31,9 +40,10 @@ class SearchHeader: UITableViewHeaderFooterView {
     
     func setupAutoLayout(){
         
-        contentView.addSubviews([logoImage,searchTextField])
+        contentView.addSubviews([logoImage,searchTextField,searchIconImage])
         logoImage.translatesAutoresizingMaskIntoConstraints = false
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchIconImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
                    logoImage.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 16),
@@ -48,12 +58,21 @@ class SearchHeader: UITableViewHeaderFooterView {
                    searchTextField.trailingAnchor.constraint(equalTo:
                                                                 contentView.trailingAnchor,constant: -29),
                    searchTextField.heightAnchor.constraint(equalToConstant: 48),
-                   searchTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+                   searchTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                   
+                   searchIconImage.trailingAnchor.constraint(equalTo: searchTextField.trailingAnchor,constant: -2),
+                   searchIconImage.centerYAnchor.constraint(equalTo: searchTextField.centerYAnchor)
                ])
         
         searchTextField.layer.borderColor = UIColor.gray.cgColor
         searchTextField.layer.borderWidth = 1.0
         searchTextField.layer.cornerRadius = 25
+        
+        
+        
+        
+        
+        
         
         
         
