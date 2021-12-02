2d9bc243f4b9199a9135bbbbff48bf67a9d291b4
heerucan <iwpow0307@gmail.com>
2021-12-02 20:11:43 +0900

    [ADD] MainView 서버통신 설정 파일 세팅(#26)

---
+++ Tabling-iOS/Source/Network/APIConstant.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstant.swift
+//  Tabling-iOS
+//
+//  Created by Thisisme Hi on 2021/12/02.
+//
+
+import Foundation
+
+struct APIConstant {
+    
+    static let baseURL = "https://asia-northeast3-joint-seminar-tabling.cloudfunctions.net/api"
+    
+    static let home = baseURL + "/home/cafe"
+}
---
+++ Tabling-iOS/Source/Network/Services/NJMainService.swift
@@ -0,0 +1,8 @@
+//
+//  NJMainService.swift
+//  Tabling-iOS
+//
+//  Created by Thisisme Hi on 2021/12/02.
+//
+
+import Foundation
